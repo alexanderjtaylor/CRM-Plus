1d886e019ffc3255306fe034f7be6cf785925806
Alex <alexandertaylor315@gmail.com>
2023-05-27 17:24:07 -0400

    created POST request in home function in views.py file to login user and show message if user is authenticated and show a message if they are not + created logout_user function in views.py that takes request and logs user out and promps message confirming and redircts to home + updated navbar.html to check if user is authenticated and if yes a logout with link to url logout is present and if not a login link to url home is present + updated base.html to check for messages and display if present

--- website/views.py
+++ website/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You've been logged in!")
+            return redirect('home')
+        else:
+            messages.success(request, "There was an error logging in. Please try again.")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You've been logged out...")
+    return redirect('home')
+    
