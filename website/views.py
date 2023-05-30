ab949f6cc686d3fced20d8f7859a2d7b45a692cd
Alex <alexandertaylor315@gmail.com>
2023-05-30 15:44:23 -0400

    created else condition for home function in views.py to display records + in home.html created table to display record info for user such as name, email, phone, etc.

--- website/views.py
+++ website/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+    
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -16,7 +19,7 @@ def home(request):
             messages.success(request, "There was an error logging in. Please try again.")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 
 
